d6b8bf36df57c5b5e6052c1904f7c58a74e748b6
Sergey <xxsmotur@gmail.com>
2019-12-27 02:26:40 +0500

    updating of go mod

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/hashicorp/consul/api v1.3.0
-	github.com/saromanov/antenna v0.0.0-20191224133544-9d83041542d0
 	github.com/urfave/cli/v2 v2.1.1
+	gopkg.in/yaml.v2 v2.2.7
 )
--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"os"
 
-	"github.com/saromanov/antenna/command/client"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := &cli.App{
 		Name:  "diselfuel",
-		Usage: "make an explosive entrance",
+		Usage: "Starting of the app",
 		Commands: []*cli.Command{
 			{
 				Name:    "start",
