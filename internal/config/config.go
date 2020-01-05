34f9564ccb119d3ac66c51c9fdb760693f94cb4e
Sergey <xxsmotur@gmail.com>
2020-01-05 18:14:44 +0500

    Add method for make default

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,33 +3,55 @@ package config
 import (
 	"fmt"
 	"io/ioutil"
+
 	"gopkg.in/yaml.v2"
 )
 
 // Config provides definition of configuration
 type Config struct {
-	Sefvers []Server `yaml:"servers"`
+	Servers []Server `yaml:"servers"`
 }
 
 // Server provides definition of the server for configuration
 // It should contains Name and Addrss of the server
 type Server struct {
-	Name string `yaml:"name"`
+	Name    string `yaml:"name"`
 	Address string `yaml:"address"`
+	Port    int    `yaml:"port"`
+}
+
+// makeDefault filling default attributes at the config
+func (c *Config) makeDefault() {
+	if c == nil {
+		c = &Config{}
+	}
+	if len(c.Servers) == 0 {
+		c = &Config{
+			Servers: []Server{
+				Server{
+					Name:    "default",
+					Address: "localhost",
+					Port:    8081,
+				},
+			},
+		}
+	}
 }
 
 // Load provides loading of the config
-func Load(path string)(*Config, error) {
+func Load(path string) (*Config, error) {
 
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-    	return nil, fmt.Errorf("unable to load config file: %v", err)
+		return nil, fmt.Errorf("unable to load config file: %v", err)
 	}
 
 	c := &Config{}
 	err = yaml.Unmarshal([]byte(data), &c)
 	if err != nil {
-			return nil, fmt.Errorf("unable to unmarshal config file: %v", err)
+		return nil, fmt.Errorf("unable to unmarshal config file: %v", err)
 	}
+
+	c.makeDefault()
 	return c, nil
 }
