33c4b2c1511f44bd37449df0ff60e1f20dd76142
Sergey <xxsmotur@gmail.com>
2020-01-11 23:57:01 +0500

    small update

--- internal/exec/exec.go
+++ internal/exec/exec.go
@@ -5,16 +5,33 @@ package exec
 import (
 	"bytes"
 	"fmt"
+
 	"golang.org/x/crypto/ssh"
 )
-func executeCommand(command, hostname string, port string, config *ssh.ClientConfig) string {
-    conn, _ := ssh.Dial("tcp", fmt.Sprintf("%s:%s", hostname, port), config)
-    session, _ := conn.NewSession()
-    defer session.Close()
 
-    var stdoutBuf bytes.Buffer
-    session.Stdout = &stdoutBuf
-    session.Run(command)
+type Execute struct {
+}
 
-    return fmt.Sprintf("%s -> %s", hostname, stdoutBuf.String())
+func (e *Execute) Do() error {
+	_, err := executeCommand()
+	return err
+}
+func executeCommand(command, hostname string, port string, config *ssh.ClientConfig) (string, error) {
+	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", hostname, port), config)
+	if err != nil {
+		return "", fmt.Errorf("unable to connect: %v", err)
+	}
+	session, err := conn.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("unable to make session: %v", err)
+	}
+	defer session.Close()
+
+	var stdoutBuf bytes.Buffer
+	session.Stdout = &stdoutBuf
+	if err := session.Run(command); err != nil {
+		return "", fmt.Errorf("unable to run command: %v", err)
+	}
+
+	return fmt.Sprintf("%s -> %s", hostname, stdoutBuf.String()), nil
 }
