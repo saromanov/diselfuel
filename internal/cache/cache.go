93f28d4b4e0a905919aeca5f9b271c9120ecbfb4
Sergey <xxsmotur@gmail.com>
2020-01-21 16:02:58 +0500

    fix typos

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,8 @@
 package cache
 
 import (
+	"time"
+
 	"github.com/patrickmn/go-cache"
 )
 
@@ -10,16 +12,18 @@ type Cache struct {
 }
 
 // New provides initialization of the cache
-func New()*Cache {
-	return &Cache {
+func New() *Cache {
+	return &Cache{
 		c: cache.New(5*time.Minute, 10*time.Minute),
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+// Set provides inserting to the cache
+func (c *Cache) Set(key string, value interface{}) error {
 	return c.Set(key, value)
 }
 
-func (c *Cache) Get(key string)(interface{}, error) {
-	return c.Get(key, value)
+// Get provides getting from the cache
+func (c *Cache) Get(key string) (interface{}, error) {
+	return c.Get(key)
 }
