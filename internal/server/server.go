59dadc94f543ac9f6913a269820effc7e58a6fe3
Sergey <xxsmotur@gmail.com>
2020-02-05 00:21:00 +0500

    Register payload

--- internal/server/server.go
+++ internal/server/server.go
@@ -24,6 +24,7 @@ func New(a *app.App, c *config.Config, log *logrus.Logger) {
 	r.Get("/v1/info", s.Info)
 	r.Get("/v1/nodes", s.List)
 	r.Get("/v1/exec", s.Exec)
+	r.Post("/v1/apply", s.Apply)
 
 	log.Infof("starting of the server at %s:%d", c.Master.Address, c.Master.Port)
 	server := lucio.NewServer(r, c.Master.Address, c.Master.Port)
